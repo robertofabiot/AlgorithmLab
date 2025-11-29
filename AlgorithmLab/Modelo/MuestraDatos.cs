4325ee656646ab431eb1753eb303421cec75e971
robertofabiot <rftercerom@uamv.edu.ni>
2025-11-29 13:56:20 -0600

    Add models

---
+++ AlgorithmLab/Modelo/MuestraDatos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmLab.Modelo
+{
+    public class MuestraDatos
+    {
+        // Soporta int, float y string
+        public IComparable[] Datos { get; set; }
+        public string TipoDato { get; set; } // "Entero", "Decimal", "Texto"
+        public bool EstaOrdenado { get; set; } = false;
+    }
+}
---
+++ AlgorithmLab/Modelo/RegistroBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AlgorithmLab.Modelo
+{
+    public class RegistroBenchmark
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string TipoOperacion { get; set; } // "Ordenamiento" o "Búsqueda"
+        public string Algoritmo { get; set; }
+        public string TiempoEjecucion { get; set; } // Formato 00:00:00.000
+        public double TiempoMilisegundos { get; set; } // Para gráficas
+        public long MemoriaPicoBytes { get; set; }
+        public long Iteraciones { get; set; }
+        public string TipoDato { get; set; }
+        public int TamanoMuestra { get; set; }
+
+        // Para búsqueda
+        public bool SeOrdenoPreviamente { get; set; }
+        public string ValorBuscado { get; set; }
+        public int IndiceEncontrado { get; set; }
+    }
+}
